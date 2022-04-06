3911e50b763f10510721b0b105588f3cbac4e15a
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-06 14:16:18 -0400

    Why are all these files here?

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import './App.css';
 import Tricks from "../Tricks/Tricks";
 import Card from "../Card/Card";
+import Form from "../Form/Form";
 import { getTrickData } from "../../apiCalls";
 
 class App extends Component {
@@ -17,10 +18,15 @@ class App extends Component {
     .then((data) => this.setState({ tricks: data }))
   }
 
+  addTrick = (newTrick) => {
+    this.setState({ tricks: [...this.state.tricks, newTrick] });
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <Form addTrick={this.addTrick}/>
         <Tricks tricks={this.state.tricks} />
       </div>
     )
