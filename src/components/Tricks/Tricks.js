da55a68efecf5249dec96d5ee2d3cb2b37585d4c
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-06 12:51:47 -0400

    Add Card to return

--- src/components/Tricks/Tricks.js
+++ src/components/Tricks/Tricks.js
@@ -1,12 +1,23 @@
-import React from "react"
-import "./Tricks.css"
+import React from "react";
+import Card from "../Card/Card";
+import "./Tricks.css";
 
 const Tricks = ( {tricks} ) => {
-  const trickCards = tricks.map()
-  return(
-    <div className="card">
-      <h1>CARD</h1>
+  
+  const trickCards = tricks.map(trick => {
+    return (
+      <Card
+        name={trick.name}
+        obstacle={trick.obstacle}
+        tutorial={trick.tutorial}
+        id={trick.id}
+      />
+    )
+  })
 
+  return(
+    <div className="trick-card">
+      {trickCards}
     </div>
   )
 }
