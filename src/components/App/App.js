294f2465098a282187280fcc3c7b59d4aeed4929
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-06 12:53:08 -0400

    Fixed my componentDidMount: I DID NOT NEED DATA.TRICKS. MAKE SURE TO REVIEW THIS EMILY

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,29 @@
 import { Component } from 'react';
 import './App.css';
-import Cards from "../../Cards.js"
+import Tricks from "../Tricks/Tricks";
+import Card from "../Card/Card";
+import { getTrickData } from "../../apiCalls";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tricks: []
+    }
+  }
+
+  componentDidMount() {
+    getTrickData()
+    .then((data) => this.setState({ tricks: data }))
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Sick Trick Wish List</h1>
+        <Tricks tricks={this.state.tricks} />
       </div>
-    );
+    )
   }
 }
 
