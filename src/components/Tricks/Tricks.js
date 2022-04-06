063a10a85f62889a2fc9b2ea41d1ba4250dbc1b2
Emily Corcoran <cathleencorcoran@gmail.com>
2022-04-06 12:12:13 -0400

    Add Tricks file

---
+++ src/components/Tricks/Tricks.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./Tricks.css"
+
+const Tricks = ( {tricks} ) => {
+  const trickCards = tricks.map()
+  return(
+    <div className="card">
+      <h1>CARD</h1>
+
+    </div>
+  )
+}
+
+
+export default Tricks;
